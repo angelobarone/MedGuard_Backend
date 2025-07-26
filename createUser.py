4346d7867032f3b98c7b2a924e194e33f653b290
angelobarone <46626188+angelobarone@users.noreply.github.com>
2025-07-26 16:14:46 +0200

    working on interazione tra frontend e backend sull'autenticazione dell'utente

--- createUser.py
+++ createUser.py
@@ -3,6 +3,6 @@ import models
 
 username = input("Inserisci username: ")
 password = input("Inserisci password: ")
-type = input("Inserisci tipo (A = client, B = stat, C = admin): ")
+type = input("Inserisci tipo (A = hospital, S = authorized, C = admin): ")
 
 models.create_user(username, password, type)
